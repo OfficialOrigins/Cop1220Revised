104362a5589702e58f34e8ec91528bf15857e30d
Arav Halade <aravhalade@gmail.com>
2025-01-23 11:18:37 -0500

    Initial commit

---
+++ src/lyrics.c
@@ -0,0 +1,36 @@
+//
+// Created by aravh on 1/23/2025.
+//
+
+// //Module 1-2 Lyrics 01/19/25
+//
+// #include <stdio.h>
+//
+// int main(void){
+//
+//     //Unit test 1
+//     printf("oh, well imagine /\n");
+//     printf("As I'm pacing the pews in a church corridor /\n");
+//     printf("And I can't help but to hear /\n");
+//     printf("No, I can't help but to hear an exchanging of words...\n");
+//
+//     //Unit test 2
+//     printf("Oh-oh-oh-oh-oh,\n");
+//     printf("oh-oh-oh-oh,\n");
+//     printf("oh-oh-oh /\n");
+//     printf("Caught in a bad romance...\n");\
+//
+//     //Unit test 3
+//     printf("Coming out of my cage\n");
+//     printf("And I've been doing just fine\n");
+//     printf("Gotta gotta be down\n");
+//     printf("Because I want it all\n");
+//     return 0;
+//
+// //     //Unit test 4
+// //     printf("Now, this is a story all about how");
+// //     printf("My life got flipped-turned upside down\n");
+// //     printf("And I'd like to take a minute\n");
+// //     printf("Just sit right there\n");
+// //     printf("I'll tell you how I became the prince of a town called Bel-Air\n");
+// // }
---
+++ src/main.c
@@ -0,0 +1,37 @@
+//Module 1 Assignment 1-1
+#include <stdio.h>
+
+int main(void){
+
+    //Unit test 1
+    printf("Lucinda Potter");
+    printf("\n6/24/1992");
+    printf("\nwork 000-000-0101");
+    printf("\ncell 000-000-0189");
+
+    printf("\n");
+
+    //Unit test 2
+    printf("John Smith");
+    printf("\n8/15/1978");
+    printf("\nwork 000-111-0101");
+    printf("\ncell 000-111-0189");
+
+    printf("\n");
+
+    // Unit test3
+    printf("Chris Johnson");
+    printf("\n12/01/1987");
+    printf("\nwork 000-222-0101");
+    printf("\nwork 000-222-0189");
+
+    printf("\n");
+
+    // Unit test 4
+    printf("Robert Hall");
+    printf("\n2/27/1949");
+    printf("\nwork 000-444-0101");
+    printf("\ncell 000-444-0189");
+
+    return 0;
+}
