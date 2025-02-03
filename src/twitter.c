dfa67a0a1d9722013abd7eda3375a3118be1f4ed
Arav Halade <aravhalade@gmail.com>
2025-02-02 19:07:03 -0500

    HurricaneChecker.C

--- src/twitter.c
+++ src/twitter.c
@@ -1,32 +1,32 @@
-// Created by aravh on 1/25/2025.
-//Module 2 Exercise 1
-//Twitter
-
-#include <stdio.h>
-#include <string.h>
-
-int main(void){
-    //Vars
-    int y = 889;
-    int num;
-    char tweet[y];
-    int n = 1000;
-
-    printf("Enter your Tweet: ");
-    fgets(tweet, n, stdin);
-
-    num = strlen(tweet);
-
-    //printf("%d\n", num);
-
-    if (num <= y-1){
-        printf("Posted");
-    }
-
-   if (num > y-1){
-       printf("Rejected");
-   }
-
-    return 0;
-}
-
+// // Created by aravh on 1/25/2025.
+// //Module 2 Exercise 1
+// //Twitter
+//
+// #include <stdio.h>
+// #include <string.h>
+//
+// int main(void){
+//     //Vars
+//     int y = 889;
+//     int num;
+//     char tweet[y];
+//     int n = 1000;
+//
+//     printf("Enter your Tweet: ");
+//     fgets(tweet, n, stdin);
+//
+//     num = strlen(tweet);
+//
+//     //printf("%d\n", num);
+//
+//     if (num <= y-1){
+//         printf("Posted");
+//     }
+//
+//    if (num > y-1){
+//        printf("Rejected");
+//    }
+//
+//     return 0;
+// }
+//
