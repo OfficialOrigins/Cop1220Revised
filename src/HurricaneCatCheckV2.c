1063605b53a40474d5c0d7d010db49e766214e84
Arav Halade <aravhalade@gmail.com>
2025-02-26 21:35:14 -0500

    Commented all programs except HurricaneCatCheckV2

---
+++ src/HurricaneCatCheckV2.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+// //Hurricane Category Tracker
+// //COP-1220 2/02/25
+// //Module 2 Exercise 2
+// //Arav Halade
+
+void catCheck(double dod);
+
+int main(void)
+{
+    double windInput;
+
+    printf("Enter the wind speeds: ");
+
+    scanf("%lf", &windInput);
+
+    // printf("%lf",windInput);
+
+    catCheck(windInput);
+}
+
+void catCheck(double num)
+{
+    if (num >= 157)
+    {
+        printf("Category 5 hurricane");
+    }
+    //
+    else if (num >= 130 && num < 157)
+    {
+        printf("Category 4 Hurricane");
+    }
+
+    else if (num >= 111 && num < 130)
+    {
+        printf("Category 3 Hurricane");
+    }
+
+    else if (num >= 96 && num < 111)
+    {
+        printf("Category 2 Hurricane");
+    }
+
+    else if (num >= 74 && num < 96)
+    {
+        printf("Category 1 Hurricane");
+    }
+
+    else
+    {
+        printf("Not a hurricane");
+    }
+
+}
--- src/PerfectionV2.c
+++ src/PerfectionV2.c
@@ -1,46 +1,46 @@
-#include<stdio.h>
-
-
-//PerfectionV2
-//02/26/25
-//Arav Halade
-
-
-void printPerfect(int num)
-{
-    switch (num)
-    {
-    case 6:
-        printf("Perfect Number");
-        break;
-
-    case 28:
-        printf("Perfect Number");
-        break;
-
-    case 496:
-        printf("Perfect Number");
-        break;
-
-    case 8128:
-        printf("Perfect Number");
-        break;
-
-    default:
-        printf("Not a perfect number");
-        break;
-    }
-}
-
-int main(void){
-
-    int perfectNum;
-
-    printf("Enter a perfect number between 1 - 10,000: ");
-    scanf("%d",&perfectNum);
-
-    printPerfect(perfectNum);
-
-    return 0;
-}
-
+// #include<stdio.h>
+//
+//
+// //PerfectionV2
+// //02/26/25
+// //Arav Halade
+//
+//
+// void printPerfect(int num)
+// {
+//     switch (num)
+//     {
+//     case 6:
+//         printf("Perfect Number");
+//         break;
+//
+//     case 28:
+//         printf("Perfect Number");
+//         break;
+//
+//     case 496:
+//         printf("Perfect Number");
+//         break;
+//
+//     case 8128:
+//         printf("Perfect Number");
+//         break;
+//
+//     default:
+//         printf("Not a perfect number");
+//         break;
+//     }
+// }
+//
+// int main(void){
+//
+//     int perfectNum;
+//
+//     printf("Enter a perfect number between 1 - 10,000: ");
+//     scanf("%d",&perfectNum);
+//
+//     printPerfect(perfectNum);
+//
+//     return 0;
+// }
+//
